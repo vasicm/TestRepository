6cecb30e5ff246a41e8a770b0a619a86abab4743
Marko Vasic <marko.vasic@nfcsb.com>
2018-07-23 12:09:13 +0200

    Added function declarations

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,6 +10,31 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>(){2, 5, 6, 22, 13};
+            System.Console.WriteLine("Before sorting");
+            PrintList(list);
+            ListSort(list);
+            System.Console.WriteLine("After sorting");
+            PrintList(list);
+            System.Console.Read();
+        }
+
+        public static void PrintList(List<int> list)
+        {
+            foreach (var el in list)
+            {
+                System.Console.WriteLine(el);
+            }
+        }
+
+        public static long GetFibonacciNumber(int a)
+        {
+            return 1;
+        }
+
+        public static void ListSort(List<int> list)
+        {
+
         }
     }
 }
